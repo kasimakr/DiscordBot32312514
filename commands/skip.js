760cefbc6ffd03e6fe16a0304c1e99ea8850e3b0
kasimakr <64462692+kasimakr@users.noreply.github.com>
2020-09-24 12:54:57 -0500

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,18 +1,20 @@
+const { MessageEmbed } = require("discord.js");
 const sendError = require("../util/error");
 
 module.exports = {
   info: {
-    name: "skip",
-    description: "Skips Current Song To Next Song In Queue",
+    name: "stop",
+    description: "Stops All Music",
     usage: "",
-    aliases: ["s"],
+    aliases: [],
   },
 
   run: async function (client, message, args) {
     const channel = message.member.voice.channel
-    if (!channel)return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
+    if (!channel)return sendError("Join A God Dam Music Channel Then Try Again.", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue)return sendError("There is nothing playing that I could skip for you.", message.channel);
-    serverQueue.connection.dispatcher.end("Skiped the music");
+    if (!serverQueue)return sendError("When Life Give You Lemons, You Make Guacomole. Your Not Listening To Music", message.channel);
+    serverQueue.songs = [];
+    serverQueue.connection.dispatcher.end("Stop the music");
   },
 };
