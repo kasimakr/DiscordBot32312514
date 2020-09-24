329f247ae63c3fb5e3a7150132c45ecf0704933d
kasimakr <64462692+kasimakr@users.noreply.github.com>
2020-09-24 12:52:44 -0500

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,26 +1,25 @@
 const { MessageEmbed } = require("discord.js");
-const sendError = require("../util/error")
+const sendError = require("../util/error");
 
 module.exports = {
   info: {
-    name: "nowplaying",
-    description: "Shows Music Playing In Server",
+    name: "pause",
+    description: "Pauses Current Song Playing",
     usage: "",
-    aliases: ["np"],
+    aliases: [""],
   },
 
   run: async function (client, message, args) {
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
-    let song = serverQueue.songs[0]
-    let thing = new MessageEmbed()
-      .setAuthor("Now Playing", "https://raw.githubusercontent.com/kasimakr/DiscordBot32312514/master/assets/Akrr.png")
-      .setThumbnail(song.img)
-      .setColor("BLUE")
-      .addField("Name", song.title, true)
-      .addField("Duration", song.duration, true)
-      .addField("Requested by", song.req.tag, true)
-      .setFooter(`Views: ${song.views} | ${song.ago}`)
-    return message.channel.send(thing)
+    if (serverQueue && serverQueue.playing) {
+      serverQueue.playing = false;
+      serverQueue.connection.dispatcher.pause();
+      let xd = new MessageEmbed()
+      .setDescription("Paused the music for you!")
+      .setColor("YELLOW")
+      .setAuthor("Music has been paused!", "https://raw.githubusercontent.com/kasimakr/DiscordBot32312514/master/assets/Akrr.png")
+      return message.channel.send(xd);
+    }
+    return sendError("There is nothing playing in this server.", message.channel);
   },
 };
