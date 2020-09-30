cb4dc10dfca171d52a9ca0d44d93b70621d2ebb9
kasimakr <64462692+kasimakr@users.noreply.github.com>
2020-09-29 20:18:40 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,12 @@
 const { MessageEmbed } = require('discord.js')
+const sendError = require("../util/error");
 
 module.exports = {
     info: {
-        name: "help",
-        description: "All Commands You Can Do With The Music Side Of This Bot",
+        name: "Help",
+        description: "Shows all commands bot can do",
         usage: "",
-        aliases: ["commands", "help me", "pls help"]
+        aliases: ["commands", "help me", "hell", "cmds", "h"]
     },
 
     run: async function(client, message, args){
@@ -13,24 +14,30 @@ module.exports = {
 
         client.commands.forEach(cmd => {
             let cmdinfo = cmd.info
-            allcmds+="``"+client.config.prefix+cmdinfo.name+" "+cmdinfo.usage+"`` ~ "+cmdinfo.description+"\n"
-        })
+            allcmds+="``"+client.config.prefix+cmdinfo.name+""+cmdinfo.usage+"`` → "+cmdinfo.description+"\n"
+})
 
+        message.react('759498631069171742')
         let embed = new MessageEmbed()
         .setAuthor("Commands", "https://raw.githubusercontent.com/kasimakr/DiscordBot32312514/master/assets/Akrr.png")
         .setColor("BLUE")
-        .setDescription(allcmds)
-        .setFooter(`Version: 1.0.04`)
+        .setDescription(`**Music**
+${allcmds}
+**Moderation"Not Done"**
+${"`Kick`"} → Kicks the dude (NOT DONE MY GUY)
+
+`)
+        .setFooter(`Version: 1.0.25`)
 
         if(!args[0])return message.channel.send(embed)
         else {
             let cmd = args[0]
             let command = client.commands.get(cmd)
             if(!command)command = client.commands.find(x => x.info.aliases.includes(cmd))
-            if(!command)return message.channel.send("Unknown Command")
+            if(!command)return sendError("Couldn't Find Command", message.channel, message.react('759498707774734407'), message.react)
             let commandinfo = new MessageEmbed()
-            .setTitle("Command: "+command.info.name+" info")
-            .setColor("YELLOW")
+            .setTitle("Command: "+command.info.name+"info")
+            .setColor("BLUE")
             .setDescription(`
 Name: ${command.info.name}
 Description: ${command.info.description}
