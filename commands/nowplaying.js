a48ff659e23e42e1c0735de2efb2ef240a411349
kasimakr <64462692+kasimakr@users.noreply.github.com>
2020-09-29 20:20:06 -0500

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,25 +1,104 @@
-const { MessageEmbed } = require("discord.js");
-const sendError = require("../util/error");
+const { Util, MessageEmbed } = require("discord.js");
+const ytdl = require("ytdl-core");
+const yts = require("yt-search");
+const sendError = require("../util/error")
 
 module.exports = {
   info: {
-    name: "pause",
-    description: "Pauses Current Song Playing",
+    name: "NewSong",
+    description: "Adds new songs to the queue",
     usage: "",
-    aliases: [""],
+    aliases: ["AS"],
   },
 
   run: async function (client, message, args) {
-    const serverQueue = message.client.queue.get(message.guild.id);
-    if (serverQueue && serverQueue.playing) {
-      serverQueue.playing = false;
-      serverQueue.connection.dispatcher.pause();
-      let xd = new MessageEmbed()
-      .setDescription("Paused the music for you!")
-      .setColor("YELLOW")
-      .setAuthor("Music has been paused!", "https://raw.githubusercontent.com/kasimakr/DiscordBot32312514/master/assets/Akrr.png")
-      return message.channel.send(xd);
+    const channel = message.member.voice.channel;
+    if (!channel)return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
+
+    const permissions = channel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT"))return sendError("I cannot connect to your voice channel, make sure I have the proper permissions!", message.channel);
+    if (!permissions.has("SPEAK"))return sendError("I cannot speak in this voice channel, make sure I have the proper permissions!", message.channel);
+
+    var searchString = args.join(" ");
+    if (!searchString)return sendError("You didn't poivide want i want to play", message.channel);
+
+    var serverQueue = message.client.queue.get(message.guild.id);
+
+    var searched = await yts.search(searchString)
+    if(searched.videos.length === 0)return sendError("Looks like i was unable to find the song on YouTube", message.channel)
+    var songInfo = searched.videos[0]
+
+    const song = {
+      id: songInfo.videoId,
+      title: Util.escapeMarkdown(songInfo.title),
+      views: String(songInfo.views).padStart(10, ' '),
+      url: songInfo.url,
+      ago: songInfo.ago,
+      duration: songInfo.duration.toString(),
+      img: songInfo.image,
+      req: message.author
+    };
+
+    if (serverQueue) {
+      message.react('759498631069171742')
+      serverQueue.songs.push(song);
+      let thing = new MessageEmbed()
+      .setAuthor("Up soon", "https://raw.githubusercontent.com/kasimakr/DiscordBot32312514/master/assets/Akrr.png")
+      .setThumbnail(song.img)
+      .setColor("PURPLE")
+      .addField("Song:", song.title, true)
+      .setFooter(`Requested By: ${song.req.tag}  Uploaded: ${song.ago}  Duration: ${song.duration}`)
+      return message.channel.send(thing);
     }
-    return sendError("There is nothing playing in this server.", message.channel);
-  },
+
+    const queueConstruct = {
+      textChannel: message.channel,
+      voiceChannel: channel,
+      connection: null,
+      songs: [],
+      volume: 2,
+      playing: true,
+    };
+    message.client.queue.set(message.guild.id, queueConstruct);
+    queueConstruct.songs.push(song);
+
+    const play = async (song) => {
+      const queue = message.client.queue.get(message.guild.id);
+      if (!song) {
+        sendError("Song Finished", message.channel)
+        queue.voiceChannel.leave();//If you want your bot stay in vc 24/7 remove this line :D
+        message.client.queue.delete(message.guild.id);
+        return;
+      }
+
+      const dispatcher = queue.connection
+        .play(ytdl(song.url))
+        .on("finish", () => {
+          queue.songs.shift();
+          play(queue.songs[0]);
+        })
+        .on("error", (error) => console.error(error));
+      dispatcher.setVolumeLogarithmic(queue.volume / 5);
+      let thing = new MessageEmbed()
+      .setAuthor("Music Starting", "https://raw.githubusercontent.com/kasimakr/DiscordBot32312514/master/assets/Akrr.png")
+      .setThumbnail(song.img)
+      .setColor("BLUE")
+      .addField("Song:", song.title, true)
+      .setFooter(`Requested By: ${song.req.tag}  Uploaded: ${song.ago}  Duration: ${song.duration}`)
+      queue.textChannel.send(thing);
+      message.react('759498631069171742')
+    };
+
+    try {
+      const connection = await channel.join();
+      queueConstruct.connection = connection;
+      channel.guild.voice.setSelfDeaf(false)
+      play(queueConstruct.songs[0]);
+    } catch (error) {
+      console.error(`I could not join the voice channel: ${error}`);
+      message.client.queue.delete(message.guild.id);
+      await channel.leave();
+      return sendError(`I could not join the voice channel: ${error}`, message.channel);
+    }
+  }
 };
