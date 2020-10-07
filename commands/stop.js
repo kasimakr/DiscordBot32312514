3a8248cd00c7ff8b921dbc5f9c8b6c7b7d4e373c
kasimakr <64462692+kasimakr@users.noreply.github.com>
2020-10-07 14:42:00 -0500

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -16,5 +16,12 @@ module.exports = {
     if (!serverQueue)return sendError("When Life Give You Lemons, You Make Guacomole. Your Not Listening To Music", message.channel, message.react('759498707774734407'));
     serverQueue.songs = [];
     serverQueue.connection.dispatcher.end("Stop the music");
+      let Embed = new MessageEmbed()
+      .setDescription("Music Stopped")
+      .setColor("RED")
+      .setAuthor("Music Stopped", "https://raw.githubusercontent.com/kasimakr/DiscordBot32312514/master/assets/KasimAkrSad.png")
+      .setFooter("Start a support ticket if you have any issues!")
+      return message.channel.send(Embed);
+
   },
 };
