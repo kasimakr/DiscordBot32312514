084deb52adf80c7651f358e574ad3f8e8db2c3b3
kasimakr <64462692+kasimakr@users.noreply.github.com>
2020-10-07 14:36:56 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Kasim Akr ;help ", {
+  await client.user.setActivity("Kasim Akr PI ;help ", {
     type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
